1ed57ecd641081539ceb772437a65927858ada9e
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-02-19 11:58:28 -0300

    Páginas home, privacidade e sobre

--- App.js
+++ App.js
@@ -1,128 +1,16 @@
-import {
-  Button,
-  Image,
-  Pressable,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import logo from "./assets/images/logo.png";
-import { Ionicons } from "@expo/vector-icons";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
+import { StatusBar, StyleSheet } from "react-native";
 
-SplashScreen.preventAutoHideAsync();
+import Home from "./src/screens/Home";
+import Sobre from "./src/screens/Sobre";
+import Privacidade from "./src/screens/Privacidade";
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    "Monoton-Regular": require("./assets/fonts/Monoton-Regular.ttf"),
-    NotoSans: require("./assets/fonts/NotoSans-VariableFont.ttf"),
-  });
-  /* Função atrelada ao hook useCallback
-  Quando uma função está conectada ao useCallback, garantidos que a referência dela é armazenada na memória somente uma vez  */
-  const aoAtualizarLayout = useCallback(async () => {
-    /* Se estiver tudo ok com o carregamento */
-    if (fontsLoaded || fontError) {
-      /* Escondemos a splashscreen */
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView style={estilos.container} onLayout={aoAtualizarLayout}>
-        <View style={estilos.viewLogo}>
-          <Image source={logo} style={estilos.logo} />
-          <Text style={estilos.titulo}>Cinéfilos</Text>
-        </View>
-
-        <View style={estilos.viewBotoes}>
-          <Pressable style={estilos.botao}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="search" size={12} color="white" /> Buscar Filmes
-            </Text>
-          </Pressable>
-          <Pressable style={estilos.botao}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="star" size={12} color="gold" /> Favoritos
-            </Text>
-          </Pressable>
-        </View>
-
-        <View style={estilos.viewRodape}>
-          <Pressable style={estilos.botaoRodape}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="lock-closed" size={12} color="white" />{" "}
-              Privacidade
-            </Text>
-          </Pressable>
-          <Pressable style={estilos.botaoRodape}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="information-circle" size={12} color="white" />{" "}
-              Sobre
-            </Text>
-          </Pressable>
-        </View>
-      </SafeAreaView>
+      {/* <Home /> */}
+      {/* <Sobre /> */}
+      <Privacidade />
     </>
   );
 }
-
-const estilos = StyleSheet.create({
-  container: {
-    backgroundColor: "#f4fcff",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 125,
-    height: 125,
-  },
-  viewLogo: {
-    flex: 3,
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-  viewBotoes: {
-    gap: 20,
-    flex: 2,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  botao: {
-    padding: 16,
-    backgroundColor: "#ff4117",
-    borderRadius: 5,
-    width: "40%",
-    alignItems: "center",
-  },
-  textoBotao: {
-    color: "#fff",
-    fontFamily: "NotoSans",
-  },
-  titulo: {
-    fontFamily: "Monoton-Regular",
-    fontSize: 28,
-  },
-  viewRodape: {
-    width: "100%",
-    flex: 0.5,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#ff4117",
-  },
-  botaoRodape: {
-    padding: 16,
-    alignItems: "center",
-  },
-});
---
+++ src/screens/Home.js
@@ -0,0 +1,88 @@
+import { StyleSheet, Text, View, Button, Image, Pressable } from "react-native";
+import React from "react";
+import SafeContainer from "../components/SafeContainer";
+import logo from "../../assets/images/logo.png";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Home() {
+  return (
+    <SafeContainer>
+      <View style={estilos.viewLogo}>
+        <Image source={logo} style={estilos.logo} />
+        <Text style={estilos.titulo}>Cinéfilos</Text>
+      </View>
+
+      <View style={estilos.viewBotoes}>
+        <Pressable style={estilos.botao}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="search" size={12} color="white" /> Buscar Filmes
+          </Text>
+        </Pressable>
+        <Pressable style={estilos.botao}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="star" size={12} color="gold" /> Favoritos
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={estilos.viewRodape}>
+        <Pressable style={estilos.botaoRodape}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="lock-closed" size={12} color="white" /> Privacidade
+          </Text>
+        </Pressable>
+        <Pressable style={estilos.botaoRodape}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="information-circle" size={12} color="white" /> Sobre
+          </Text>
+        </Pressable>
+      </View>
+    </SafeContainer>
+  );
+}
+
+const estilos = StyleSheet.create({
+  logo: {
+    width: 125,
+    height: 125,
+  },
+  viewLogo: {
+    flex: 3,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  viewBotoes: {
+    gap: 20,
+    flex: 2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  botao: {
+    padding: 16,
+    backgroundColor: "#ff4117",
+    borderRadius: 5,
+    width: "40%",
+    alignItems: "center",
+  },
+  textoBotao: {
+    color: "#fff",
+    fontFamily: "NotoSans",
+  },
+  titulo: {
+    fontFamily: "Monoton-Regular",
+    fontSize: 28,
+  },
+  viewRodape: {
+    width: "100%",
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#ff4117",
+  },
+  botaoRodape: {
+    padding: 16,
+    alignItems: "center",
+  },
+});
