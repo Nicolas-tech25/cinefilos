c6f7f8b6eb9fbbfb9d2e58550b6dddf1c79ddaa8
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-03-04 12:03:27 -0300

    cddcd

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,10 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 import SafeContainer from "../components/SafeContainer";
+import { api, apikey } from "../services/api-moviedb";
+import { useEffect, useState, React } from "react";
 
 export default function Resultados({ route }) {
+  // const terror = 27;
+  const [resultados, setResultados] = useState([]);
   // Capturando o parâmetro filme vindo de BuscarFilmes
   const { filme } = route.params;
+
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get(`/search/movie`, {
+          params: {
+            include_adult: true,
+            language: "pt-BR",
+            query: filme,
+            api_key: apikey,
+          },
+        });
+        console.log(resposta.data.results);
+      } catch (error) {
+        console.error("Deu ruim: " + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
